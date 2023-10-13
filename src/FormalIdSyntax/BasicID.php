cf1353496d911b72304c9a2ceedadf3c919aa91b
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2023-10-13 13:30:02 +0200

    Add format for basicID and Spec

--- src/FormalIdSyntax/BasicID.php
+++ src/FormalIdSyntax/BasicID.php
@@ -8,5 +8,24 @@ namespace PrinsFrank\TransliteratorWrapper\FormalIdSyntax;
  */
 class BasicID
 {
+    public function __construct(
+        public readonly Spec $specA,
+        public readonly Spec|null $specB = null,
+        public readonly Identifier|null $identifier = null,
+    ) {
+    }
 
+    public function __toString(): string
+    {
+        $string = $this->specA->__toString();
+        if ($this->specB !== null) {
+            $string .= '-' . $this->specB->__toString();
+        }
+
+        if ($this->identifier !== null) {
+            $string .= '/' . $this->identifier->__toString();
+        }
+
+        return $string;
+    }
 }
--- src/FormalIdSyntax/Spec.php
+++ src/FormalIdSyntax/Spec.php
@@ -3,12 +3,18 @@ declare(strict_types=1);
 
 namespace PrinsFrank\TransliteratorWrapper\FormalIdSyntax;
 
+use PrinsFrank\Standards\Scripts\ScriptName;
+
 /**
  * @see https://unicode-org.github.io/icu/userguide/transforms/general/#formal-id-syntax
  */
 class Spec
 {
     public function __construct(
-
+        public readonly ScriptName|LocaleName|Identifier $scriptLocaleOrIdentifier,
     ) { }
+
+    public function __toString(): string
+    {
+    }
 }
