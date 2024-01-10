2cbb4625c03fa869c6317440400ad6a0a31a0bc6
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-01-10 21:10:15 +0100

    Update csfixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -10,7 +10,15 @@ return (new PhpCsFixer\Config())->setRules(
         'no_extra_blank_lines'         => true,
         'class_attributes_separation'  => true,
         'array_syntax'                 => ['syntax' => 'short'],
-        'binary_operator_spaces'       => ['default' => 'align_single_space']
+        'binary_operator_spaces'       => ['default' => 'align_single_space'],
+        'no_unused_imports'            => true,
+        'array_indentation'            => true,
+        'no_alias_functions'           => true,
+        'phpdoc_line_span'             => ['property' => 'single', 'method' => 'single', 'const' => 'single'],
+        'phpdoc_order'                 => ['order' => ['deprecated', 'internal', 'standard', 'source', 'template', 'implements', 'extends', 'covers', 'coversDefaultClass', 'param', 'throws', 'return']],
+        'phpdoc_param_order'           => true,
+        'phpdoc_separation'            => ['groups' => [['deprecated', 'internal'], ['standard', 'source'], ['covers', 'coversDefaultClass'], ['template', 'implements', 'extends', 'param', 'throws', 'return']]],
+        'phpdoc_trim'                  => true,
     ]
 )->setFinder(
     PhpCsFixer\Finder::create()
