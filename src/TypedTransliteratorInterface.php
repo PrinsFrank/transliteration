a0f451190784ecf3f210846c054d591ec97a5e8e
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-01-13 13:47:32 +0100

    Update interface

--- src/TypedTransliteratorInterface.php
+++ src/TypedTransliteratorInterface.php
@@ -6,13 +6,14 @@ namespace PrinsFrank\Transliteration;
 use PrinsFrank\Transliteration\Enum\TransliterationDirection;
 use PrinsFrank\Transliteration\FormalIdSyntax\CompoundID;
 use PrinsFrank\Transliteration\FormalIdSyntax\SingleID;
+use PrinsFrank\Transliteration\Rule\RuleList;
 use Transliterator;
 
 interface TypedTransliteratorInterface
 {
-    public function create(SingleID|CompoundID $id, TransliterationDirection $direction = TransliterationDirection::FORWARD): Transliterator;
+    public function create(SingleID|CompoundID|RuleList $id, TransliterationDirection $direction = TransliterationDirection::FORWARD): Transliterator;
 
-    public function transliterate(string $string, SingleID|CompoundID $id, TransliterationDirection $direction = TransliterationDirection::FORWARD): string;
+    public function transliterate(string $string, SingleID|CompoundID|RuleList $id, TransliterationDirection $direction = TransliterationDirection::FORWARD): string;
 
     /** @return list<string> */
     public function listIDs(): array;
