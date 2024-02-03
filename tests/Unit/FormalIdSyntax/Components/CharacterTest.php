666d375829a4696296fcb72d3883149abfc8bf22
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-02-03 19:35:36 +0100

    Update tests/Unit/FormalIdSyntax/Components/CharacterTest.php
    Co-authored-by: Viktor Szépe <viktor@szepe.net>

--- tests/Unit/FormalIdSyntax/Components/CharacterTest.php
+++ tests/Unit/FormalIdSyntax/Components/CharacterTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace FormalIdSyntax\Components;
+namespace PrinsFrank\Transliteration\Tests\FormalIdSyntax\Components;
 
 use PHPUnit\Framework\TestCase;
 use PrinsFrank\Transliteration\Exception\InvalidArgumentException;
