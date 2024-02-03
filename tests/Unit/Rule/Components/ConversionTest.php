3fd9eeb0956523701279e5faf2dadfe846ee984f
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-02-03 20:18:44 +0100

    Add more test cases

---
+++ tests/Unit/Rule/Components/ConversionTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace PrinsFrank\Transliteration\Tests\Unit\Rule\Components;
+
+use PHPUnit\Framework\TestCase;
+use PrinsFrank\Transliteration\Enum\TransliterationDirection;
+use PrinsFrank\Transliteration\Exception\InvalidArgumentException;
+use PrinsFrank\Transliteration\Rule\Components\Conversion;
+
+/** @coversDefaultClass \PrinsFrank\Transliteration\Rule\Components\Conversion */
+class ConversionTest extends TestCase
+{
+    /** @covers ::__construct */
+    public function testConstructThrowsExceptionWhenEmptyStringIsSuppliedForTextToReplace(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('$textToReplace should be a non-empty string');
+        new Conversion('', '');
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::__toString
+     */
+    public function testToString(): void
+    {
+        static::assertSame(
+            'a>b',
+            (new Conversion('a', 'b'))->__toString()
+        );
+        static::assertSame(
+            'a>b',
+            (new Conversion('a', 'b', conversionDirection: TransliterationDirection::FORWARD))->__toString()
+        );
+        static::assertSame(
+            'b<a',
+            (new Conversion('a', 'b', conversionDirection: TransliterationDirection::REVERSE))->__toString()
+        );
+        static::assertSame(
+            'x{a}y>b|z',
+            (new Conversion('a', 'b', 'x', 'y', 'z', TransliterationDirection::FORWARD))->__toString()
+        );
+        static::assertSame(
+            'b|z<x{a}y',
+            (new Conversion('a', 'b', 'x', 'y', 'z', TransliterationDirection::REVERSE))->__toString()
+        );
+    }
+}
