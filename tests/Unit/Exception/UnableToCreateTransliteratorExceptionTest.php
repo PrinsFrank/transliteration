e2cd03e661ab692cd3223d94e06eb93b9c5eb4ad
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-02-03 19:35:40 +0100

    Update tests/Unit/Exception/UnableToCreateTransliteratorExceptionTest.php
    Co-authored-by: Viktor Szépe <viktor@szepe.net>

--- tests/Unit/Exception/UnableToCreateTransliteratorExceptionTest.php
+++ tests/Unit/Exception/UnableToCreateTransliteratorExceptionTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Exception;
+namespace PrinsFrank\Transliteration\Tests\Unit\Exception;
 
 use PHPUnit\Framework\TestCase;
 use PrinsFrank\Transliteration\Exception\UnableToCreateTransliteratorException;
