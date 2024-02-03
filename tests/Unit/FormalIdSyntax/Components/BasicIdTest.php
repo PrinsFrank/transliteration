bde98f67d56f935e01849378be154a0bcfe716d5
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-02-03 19:26:07 +0100

    Remove invalidArgumentException from throws annotation (#13)

--- tests/Unit/FormalIdSyntax/Components/BasicIdTest.php
+++ tests/Unit/FormalIdSyntax/Components/BasicIdTest.php
@@ -5,7 +5,6 @@ namespace PrinsFrank\Transliteration\Tests\Unit\FormalIdSyntax\Components;
 
 use PHPUnit\Framework\TestCase;
 use PrinsFrank\Standards\Country\CountryAlpha2;
-use PrinsFrank\Standards\InvalidArgumentException;
 use PrinsFrank\Standards\Language\LanguageAlpha2;
 use PrinsFrank\Standards\LanguageTag\LanguageTag;
 use PrinsFrank\Standards\Scripts\ScriptName;
@@ -21,8 +20,6 @@ class BasicIdTest extends TestCase
      *
      * @covers ::__construct
      * @covers ::__toString
-     *
-     * @throws InvalidArgumentException
      */
     public function testToString(): void
     {
