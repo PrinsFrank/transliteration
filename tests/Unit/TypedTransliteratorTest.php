9bb01f29d572e492ebb236d97b36cd790d427902
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-02-04 20:37:57 +0100

    Add UT for create method on typedTransliterator

--- tests/Unit/TypedTransliteratorTest.php
+++ tests/Unit/TypedTransliteratorTest.php
@@ -4,11 +4,40 @@ declare(strict_types=1);
 namespace PrinsFrank\Transliteration\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use PrinsFrank\Standards\Scripts\ScriptAlias;
+use PrinsFrank\Transliteration\Enum\SpecialTag;
+use PrinsFrank\Transliteration\Enum\TransliterationDirection;
+use PrinsFrank\Transliteration\FormalIdSyntax\Components\BasicID;
+use PrinsFrank\Transliteration\FormalIdSyntax\SingleID;
+use PrinsFrank\Transliteration\Rule\RuleList;
 use PrinsFrank\Transliteration\TypedTransliterator;
+use Transliterator;
 
 /** @coversDefaultClass \PrinsFrank\Transliteration\TypedTransliterator */
 class TypedTransliteratorTest extends TestCase
 {
+    /** @covers ::create */
+    public function testCreate(): void
+    {
+        static::assertEquals(
+            Transliterator::create('Any-Latin;'),
+            (new TypedTransliterator())->create(new SingleID(new BasicID(ScriptAlias::Latin, SpecialTag::Any)))
+        );
+        static::assertEquals(
+            Transliterator::create('Any-Latin;', Transliterator::REVERSE),
+            (new TypedTransliterator())->create(new SingleID(new BasicID(ScriptAlias::Latin, SpecialTag::Any)), TransliterationDirection::REVERSE)
+        );
+        static::assertEquals(
+            Transliterator::createFromRules('::Any-Latin;'),
+            (new TypedTransliterator())->create(new RuleList([new SingleID(new BasicID(ScriptAlias::Latin, SpecialTag::Any))]))
+        );
+        static::assertEquals(
+            Transliterator::createFromRules('::Any-Latin;', Transliterator::REVERSE),
+
+            (new TypedTransliterator())->create(new RuleList([new SingleID(new BasicID(ScriptAlias::Latin, SpecialTag::Any))]), TransliterationDirection::REVERSE)
+        );
+    }
+
     /** @covers ::listIDs */
     public function testListIDs(): void
     {
