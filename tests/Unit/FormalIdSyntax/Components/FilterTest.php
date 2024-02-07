d90f9e3add1cb714019207090448b137fd52c500
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-02-07 15:12:41 +0100

    Inverse filters should be immutable (#21)

--- tests/Unit/FormalIdSyntax/Components/FilterTest.php
+++ tests/Unit/FormalIdSyntax/Components/FilterTest.php
@@ -30,20 +30,34 @@ class FilterTest extends TestCase
         static::assertSame('[a-zA-Z]', (new Filter())->addRange(new Character('a'), new Character('z'))->addRange(new Character('A'), new Character('Z'))->__toString());
     }
 
-    /**
-     * @covers ::inverse
-     * @covers ::__toString
-     */
+    /** @covers ::__toString */
     public function testInverse(): void
     {
-        static::assertSame('[^a]', (new Filter())->addChar(new Character('a'))->inverse()->__toString());
-        static::assertSame('[^a-z]', (new Filter())->addRange(new Character('a'), new Character('z'))->inverse()->__toString());
+        static::assertSame('[^a]', (new Filter(true))->addChar(new Character('a'))->__toString());
+        static::assertSame('[^a-z]', (new Filter(true))->addRange(new Character('a'), new Character('z'))->__toString());
     }
 
     /** @covers ::__toString */
     public function testToStringOnEmptyFilter(): void
     {
         static::assertSame('', (new Filter())->__toString());
-        static::assertSame('', (new Filter())->inverse()->__toString());
+        static::assertSame('', (new Filter(true))->__toString());
+    }
+
+    /** @covers ::getInverse */
+    public function testGetInverse(): void
+    {
+        static::assertEquals(new Filter(true), (new Filter())->getInverse());
+        static::assertEquals(new Filter(true), (new Filter(false))->getInverse());
+        static::assertEquals(new Filter(false), (new Filter(true))->getInverse());
+
+        static::assertEquals(new Filter(), (new Filter())->getInverse()->getInverse());
+        static::assertEquals(new Filter(false), (new Filter(false))->getInverse()->getInverse());
+        static::assertEquals(new Filter(true), (new Filter(true))->getInverse()->getInverse());
+
+        static::assertEquals(
+            (new Filter())->addChar(new Character('a'))->addRange(new Character('A'), new Character('Z')),
+            (new Filter())->addChar(new Character('a'))->addRange(new Character('A'), new Character('Z'))->getInverse()->getInverse(),
+        );
     }
 }
