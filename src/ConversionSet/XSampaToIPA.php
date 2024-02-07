11b1d1762822d5f4afb30adcf2fe0d75dd5403b7
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-02-07 20:08:25 +0100

    orginaze namespaces and built-in conversions are final

--- src/ConversionSet/XSampaToIPA.php
+++ src/ConversionSet/XSampaToIPA.php
@@ -3,11 +3,12 @@ declare(strict_types=1);
 
 namespace PrinsFrank\Transliteration\ConversionSet;
 
+use PrinsFrank\Transliteration\ConversionSet;
 use PrinsFrank\Transliteration\Syntax\Enum\SpecialTag;
 use PrinsFrank\Transliteration\TransliteratorBuilder;
 
 /** @api */
-class XSampaToIPA implements ConversionSet
+final class XSampaToIPA implements ConversionSet
 {
     public function apply(TransliteratorBuilder $transliteratorBuilder): void
     {
